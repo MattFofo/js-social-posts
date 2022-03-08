e22cfc21752ee51bede2e5eee47a28de714ae560
MattFofo <matteo.segattini2@gmail.com>
2022-03-08 14:32:43 +0100

    ciclo per scorrere gli oggetti dell'arrey ed appendere gli elementi creati

--- main.js
+++ main.js
@@ -56,42 +56,49 @@ const posts = [
     }
 ];
 
-
+console.log(posts[0]['author']['name'])
 
 const eleContainerPosts = document.getElementById('container');
-const elePost = document.createElement('div');
-elePost.classList.add('post');
 
-elePost.innerHTML = `
-    <div class="post__header">
-        <div class="post-meta">                    
-            <div class="post-meta__icon">
-                <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+
+
+//ciclo per scorrere gli oggetti dell'arrey 
+for (const key in posts) {
+
+    //creo il post con i dati dell'arrey di oggetti posts
+    const elePost = document.createElement('div');
+    elePost.classList.add('post');
+    elePost.innerHTML = `
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src=${posts[key]['author']['image']} alt=${posts[key]['author']['name']}>                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${posts[key]['author']['name']}</div>
+                    <div class="post-meta__time">${posts[key].created}</div>
+                </div>                    
             </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">Phil Mangione</div>
-                <div class="post-meta__time">${posts[0].created}</div>
-            </div>                    
         </div>
-    </div>
-    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-    <div class="post__image">
-        <img src="https://unsplash.it/600/300?image=171" alt="">
-    </div>
-    <div class="post__footer">
-        <div class="likes js-likes">
-            <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label">Mi Piace</span>
-                </a>
-            </div>
-            <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${posts[0].likes}</b> persone
-            </div>
+        <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+        <div class="post__image">
+            <img src=${posts[key]['media']} alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${posts[key].likes}</b> persone
+                </div>
+            </div> 
         </div> 
-    </div> 
-`;
+    `;
+    
+    eleContainerPosts.append(elePost);
 
-eleContainerPosts.append(elePost);
-
+}
